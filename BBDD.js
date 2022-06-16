5660c2b04b66d95b6f03f7381325d1495a0f9ff3
Santiago Garcia <just.b000ringsh1t@gmail.com>
2022-06-16 00:06:44 -0300

    add: simulacion de base de datos

---
+++ BBDD.js
@@ -0,0 +1,79 @@
+export const USUARIOS = [
+  {
+    id: 'ac54d8c1-ceba-4af4-af65-9c51d04535c4',
+    nombre: 'Catherine Holcomb',
+    correo: 'Andorra',
+  },
+  {
+    id: '3f179b46-9b21-46cb-a2d0-c4dbc4cdef55',
+    nombre: 'Houston Riley',
+    correo: 'Bahamas',
+  },
+  {
+    id: 'dcd79f4c-4ecc-43ec-ac7f-619d0f638d05',
+    nombre: 'Mcclure Levy',
+    correo: 'Angola',
+  },
+  {
+    id: '750b4e63-6371-4a85-878e-b999be57dc71',
+    nombre: 'Ester Kane',
+    correo: 'Turkey',
+  },
+  {
+    id: '5f25deb1-8451-4927-8474-3cbc96bd89d7',
+    nombre: 'Bryan Sampson',
+    correo: 'Liechtenstein',
+  },
+  {
+    id: 'c37b6522-7eeb-46d1-afd6-1dfbd4d0259a',
+    nombre: 'Burgess Justice',
+    correo: 'Guatemala',
+  },
+  {
+    id: 'c28ed378-ed0c-4819-a8d0-b58e4214c30d',
+    nombre: 'Mariana Woodard',
+    correo: 'Kenya',
+  },
+  {
+    id: '0939f5ba-4a84-4e1e-910e-17160cdcfdc2',
+    nombre: 'Patterson Owen',
+    correo: 'Hong Kong',
+  },
+  {
+    id: 'cba91d61-6c1e-47df-bb47-63139a40d217',
+    nombre: 'Mueller Beach',
+    correo: 'Cambodia',
+  },
+  {
+    id: 'f7fad1c4-bf81-4057-9948-87ddabee24d3',
+    nombre: 'Vinson Finley',
+    correo: 'Cameroon',
+  },
+  {
+    id: '15ba2fd0-e300-49fa-8470-8a6c525aa138',
+    nombre: 'Howard Murray',
+    correo: 'Afghanistan',
+  },
+  {
+    id: '6318f04b-ca4e-4b47-adb9-aa7e22850959',
+    nombre: 'Skinner Cannon',
+    correo: 'Honduras',
+  },
+  {
+    id: 'da0e649f-048b-41df-b6f9-a22a789f7273',
+    nombre: 'Byrd Bright',
+    correo: 'Ethiopia',
+  },
+  {
+    id: 'adaca9b1-cef2-45e6-ab2d-7c37502491d6',
+    nombre: 'Melva Moon',
+    correo: 'American Samoa',
+  },
+  {
+    id: 'b1d8ea46-5aba-48a9-822c-661078a5ecfe',
+    nombre: 'Opal Walker',
+    correo: 'Zaire',
+  },
+]
+
+export const PRODUCTOS = []
